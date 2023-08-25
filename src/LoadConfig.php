4c89b299a01080fb6b6f49ede98b69774a26d2bc
Aaron Klump <sourcecode@intheloftstudios.com>
2023-08-25 16:39:42 -0700

    Add JSON parsing for older YAML library.

--- src/LoadConfig.php
+++ src/LoadConfig.php
@@ -2,6 +2,7 @@
 
 namespace AKlump\EasyPerms;
 
+use League\MimeTypeDetection\FinfoMimeTypeDetector;
 use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Yaml\Yaml;
 
@@ -16,22 +17,37 @@ class LoadConfig {
    *   paths will be resolved relative to the configuration file directory.
    */
   public function __invoke(string $path): array {
-    // It's worth nothing that this method seems to parse JSON files too
-    // (version 5.4.23).
-    $config = Yaml::parseFile($path);
+    $config = $this->loadFile($path);
     $config = $this->ensureDefaults($config);
-
     $base_path = dirname($path);
     foreach (['default', 'writeable', 'executable'] as $type) {
       if (isset($config[$type]) && is_array($config[$type])) {
         $config[$type] = array_map(function (string $path) use ($base_path): string {
-          if (substr($path, 0, 1) === '/') {
-            throw new \RuntimeException(sprintf('Absolute paths are not allowed; \"%s\" is invalid.', $path));
+          if (Path::isAbsolute($path)) {
+            throw new \RuntimeException(sprintf('Absolute paths are not allowed in your configuration file, they must be relative to the configuration file.  \"%s\" is invalid.', $path));
           }
 
           return Path::makeAbsolute($path, $base_path);
         }, $config[$type]);
       }
+    }
+
+    return $config;
+  }
+
+  private function loadFile(string $path): array {
+    $mime = (new FinfoMimeTypeDetector())->detectMimeTypeFromPath($path);
+    switch ($mime) {
+      case 'application/json':
+        $config = json_decode(file_get_contents($path), TRUE);
+        break;
+
+      case 'text/yaml':
+        $config = Yaml::parseFile($path);
+        break;
+
+      default:
+        throw new \InvalidArgumentException(sprintf('Invalid configuration file type: %s', $mime));
     }
 
     return $config;
